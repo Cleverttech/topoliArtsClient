b8f85c56fda3952946e8e7fc9b9d3d48dc62fb69
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-05-15 21:46:11 +0200

    commented out lottie stuff2

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,18 +1,18 @@
 
-import React, { Component } from 'react'
-import loader from '../animation/loader.json'
-import LottieControlLoader from './LottieControlNotFound'
+// import React, { Component } from 'react'
+// import loader from '../animation/loader.json'
+// import LottieControlLoader from './LottieControlNotFound'
 
 
-export default class NotFound extends Component {
-  render() {
-    return (
-      <div>
-         <br></br>
-         <br></br>
-        <LottieControlLoader animation={loader} width={600} height={600}/>
-      </div>
-    )
-  }
-}
+// export default class NotFound extends Component {
+//   render() {
+//     return (
+//       <div>
+//          <br></br>
+//          <br></br>
+//         <LottieControlLoader animation={loader} width={600} height={600}/>
+//       </div>
+//     )
+//   }
+// }
 
