2d7dd313d6e7fdcb61174d4f0809a5b14b923a3a
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-05-17 17:35:46 +0200

    fixed after pull

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,29 +5,41 @@ import MyCoursesByStudent from "./MyCoursesByStudent";
 import PortfolioForm from "./PortfolioForm";
 
 class Profile extends Component {
+
+
+
+  
+
+  
+
+  
+      
   render() {
-    const {
-      onCreate,
-      onDeleteCourse,
-      onCreatePortfolio,
-      user,
-      error,
-      courses,
-    } = this.props;
-    return (
-      <div>
-        <h1>Profile page</h1>
-        <PortfolioForm onCreatePortfolio={onCreatePortfolio} /> <br></br>
-        <CoursesCreateForm onCreate={onCreate} />
-        <MyCoursesByMentor
-          error={error}
-          courses={courses}
-          user={user}
-          onDeleteCourse={onDeleteCourse}
-        />
-        <MyCoursesByStudent />
-      </div>
-    );
+    const { onCreate, onCreatePortfolio, user, courses, onSubmitPic, error, onDeleteCourse} = this.props;
+    if(!user) {
+      return <p>Loading...</p>
+    }else{
+      return (
+        <div>
+          <h1>Profile page</h1>
+          <img style={{width:'150px'}} src={user.image}alt="profpic"/>
+          <form onSubmit={onSubmitPic}>
+          <input name="img" type="file" placeholder="Select image"/>
+          <button>Submit</button>
+        </form>
+
+          <PortfolioForm onCreatePortfolio={onCreatePortfolio} /> <br></br>
+          <CoursesCreateForm onCreate={onCreate} />
+          <MyCoursesByMentor
+            error={error}
+            courses={courses}
+            user={user}
+            onDeleteCourse={onDeleteCourse}
+          />
+          <MyCoursesByStudent />
+        </div>
+      );
+    }
   }
 }
 export default Profile;
