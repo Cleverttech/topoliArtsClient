b517f1e1abddaa28a29e3b5deb8d41dc3ac9313a
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-05-21 00:12:59 +0200

    pre pull clever commit

--- src/components/PortfolioDetails.js
+++ src/components/PortfolioDetails.js
@@ -1,48 +1,56 @@
+import { Button, Card, CardContent } from "@material-ui/core";
 import axios from "axios";
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import config from "../config";
+import Carousel from './Carousel'
 
 export default class PortfolioDetails extends Component {
   state = {
     portfolio: null,
+    extraImg: null,
   };
   
   componentDidMount() {
     const artistId = this.props.match.params.artistId;
+    let vol1=[];
     axios
       .get(`${config.API_URL}/api/artists/${artistId}`)
       .then((result) => {
+        result.data.portfolio.images.map((e, i) => {
+          return vol1.push({imgPath: `${e}`})
+        })
         this.setState({
           portfolio: result.data.portfolio,
+          extraImg: vol1
         });
+        
       })
       .catch((err) => {
         console.log(err);
       });
   }
 
-  render() {
-    const portfolio = this.state.portfolio;
 
+  render() {
+    const {portfolio, extraImg} = this.state
+    
     if (portfolio == null) {
       return (<div>
-          <h1>No content yet, ask your mentor to post something here!</h1>
-          <Link to={'/'}>Back Home</Link>
+          <h1 alignText >No content yet, ask your mentor to post something here!</h1>
+          <Button variant='contained' color='secondary' component={Link} to={'/artists'}>Back Home</Button>
         </div>)
-    }else{
+    }else{  
     return (
       <div>
         <h1>Portfolio</h1>
-        <img src={portfolio.cover} alt={portfolio.cover} width="200px" />
-        <h3>Pictures for Carousel</h3>
-        {portfolio.images.map((e, i) => {
-          return (
-            <div key={e[i]}>
-              <p>{e}</p>
-            </div>
-          );
-        })}
+        <img style={{width:'70%'}} src={portfolio.cover} alt={portfolio.cover} />
+        <h3>{portfolio.title}</h3>
+        <p>{portfolio.description}</p>
+        <Carousel style={{marginTop: 'auto'}}images={extraImg}/>
+        <div style={{ margin: "auto" , display: 'flex', justifyContent: 'space-around'}}>
+        <Button variant='contained' color='secondary' component={Link} to={'/artists'}> Go Back </Button>
+        </div>
       </div>
     );
   }
