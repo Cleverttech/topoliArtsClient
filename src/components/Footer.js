999492cf6ae3e9b438427865986a9440c42fcf40
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-05-21 01:07:13 +0200

    added footer and styled

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react'
 
 export default class Footer extends Component {
     render() {
+        const imgStyle = {
+            width:'30px', 
+            margin:'-5px 15px 10px 15px',
+        }
         return (
             <div className='footer'>
              <h2 color= 'white'>Contact</h2>
-             <div className='footerinner'style={{ display:'flex', justifyContent:'space-around'}}>
-                 <a href='https://www.instagram.com/matthias_derenbach/' target="_blank"><img style={{width:'30px', margin:'15px'}} src='../assets/Instagram.png' alt='Instagram'/></a>
-                 <a href='https://www.facebook.com/friedadeinbuch'><img style={{width:'30px', margin:'15px'}} src='../assets/Link_Facebook.png' alt='Facebook'/></a>
-                 <a href='https://matthias-derenbach.com/'><img style={{width:'30px', margin:'15px'}} src='../assets/Link_Topoli.png' alt='Topoli'/></a>
-                 <a href='https://www.youtube.com/channel/UCsX-xoi4ObuuCXusqAbbGiw'><img style={{width:'30px', margin:'15px'}} src='../assets/Youtube-300x300.png' alt='Youtube'/></a>
+             <div className='footerinner'style={{ display:'flex', justifyContent:'center'}}>
+                 <a href='https://www.instagram.com/matthias_derenbach/' target="_blank"><img style={imgStyle} src='../assets/Instagram.png' alt='Instagram'/></a>
+                 <a href='https://www.facebook.com/friedadeinbuch' target="_blank"><img style={imgStyle} src='../assets/Link_Facebook.png' alt='Facebook'/></a>
+                 <a href='https://matthias-derenbach.com/' target="_blank"><img style={imgStyle} src='../assets/Link_Topoli.png' alt='Topoli'/></a>
+                 <a href='https://www.youtube.com/channel/UCsX-xoi4ObuuCXusqAbbGiw' target="_blank"><img style={imgStyle} src='../assets/Youtube-300x300.png' alt='Youtube'/></a>
                  
              </div>
             </div>
