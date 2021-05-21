4769a6dedb73f065bcdd4480f3bbd3740d6bf327
Clever Tedeku <clevertedeku@gmail.com>
2021-05-21 02:08:47 +0200

    style landing page

--- src/components/RandomPhases.js
+++ src/components/RandomPhases.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Grid, Paper} from '@material-ui/core'
+import {Grid, Paper,CardActionArea , Card, CardActions,CardContent, Typography,CardMedia} from '@material-ui/core'
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
@@ -13,6 +13,9 @@ const useStyles = makeStyles((theme) => ({
       color: theme.palette.text.secondary,
       maxWidth: '350px',
     },
+    media: {
+        height: 300,
+      },
   }));
 
 export default function RandomPhases () {
@@ -20,34 +23,67 @@ export default function RandomPhases () {
         const classes = useStyles();
         return (
             <div>
-                <div>
-                    <h1>How does this work?</h1>
-                        <div >
-                            
-                        </div>
-                        <div className={classes.root}>
-                            <Grid container spacing={3}>
-                                <Grid item xs={12} sm={4} md={4}>
-               
-                                    <img style={{maxWidth:250}} src='https://pbs.twimg.com/media/D8Dp0c5WkAAkvME.jpg' alt="purpose1"/>
-                                    <h5>You fill out the form and submit payment for the course.</h5>
-                        
-                                </Grid>
-                                <Grid item xs={12} sm={4} md={4}>
-                    
-                                    <img style={{maxWidth:250}} src='https://pbs.twimg.com/media/D8Dp0c5WkAAkvME.jpg'  alt="purpose2"/>
-                                    <h5>Once a week you work with our lecturers via ZOOM. Lessons are 2 hours for adults and 1 hour for children.</h5>
-                       
-                                </Grid>
-                                <Grid item xs={12} sm={4} md={4}>
-                      
-                                    <img style={{maxWidth:250}} src='https://pbs.twimg.com/media/D8Dp0c5WkAAkvME.jpg'  alt="purpose3"/>
-                                    <h5>In addition to the online painting lessons via ZOOM, you will receive a weekly link to one of our video courses. If you are a beginner, we have a video exercise tailored for you. For the advanced students we have more complicated online tasks.</h5>
-                      
-                                </Grid>
-                            </Grid>
-                        </div>
-
+              <h1>How does this work?</h1>
+                  <div className={classes.root}>
+                      <Grid container spacing={3}>
+                        <Grid item xs={12} sm={6} md={4}>
+                          <Card className={classes.root} elevation={0}>
+                            <CardActionArea>
+                                <CardMedia
+                                className={classes.media}
+                                image="../assets/AllBooks/Snorri2/DS8_Snorri2.jpg"
+                                title="Contemplative Reptile"
+                                />
+                                <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                  Step 1
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                You fill out the form and submit payment for the course
+                                </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                          </Card>
+                          </Grid>
+                          <Grid item xs={12} sm={6} md={4}>
+                          <Card className={classes.root} elevation={0}>
+                            <CardActionArea>
+                                <CardMedia
+                                className={classes.media}
+                                image="../assets/AllBooks/Snorri1/DS_1_Snorri.jpg"
+                                title="Contemplative Reptile"
+                                />
+                                <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                Step 2
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                Once a week you work with our artists via ZOOM. Lessons are 2 hours for adults and 1 hour for children.
+                                </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                          </Card>
+                         </Grid>
+                          <Grid item xs={12} sm={6} md={4}>
+                          <Card className={classes.root} elevation={0}>
+                            <CardActionArea>
+                                <CardMedia
+                                className={classes.media}
+                                image="../assets/AllBooks/Snorri1/DS_5_Snorri.jpg"
+                                title="Contemplative Reptile"
+                                />
+                                <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                Step 3
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                In addition to the online painting lessons via ZOOM, you will receive a weekly link to one of our video courses. If you are a beginner, we have a video exercise tailored for you. For the advanced students we have more complicated online tasks.
+                                </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                          </Card>
+                         </Grid>
+                      </Grid>
                 </div>
             </div>
         )
