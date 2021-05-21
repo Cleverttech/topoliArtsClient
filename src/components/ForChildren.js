eed0289cb1e438eff6e5ae81228b7c8e14053e95
Clever Tedeku <clevertedeku@gmail.com>
2021-05-21 09:26:15 +0200

    updates image file path

--- src/components/ForChildren.js
+++ src/components/ForChildren.js
@@ -75,77 +75,77 @@ function ForChildren() {
   ]
   const kidsPaintings = [
     {    
-      imgPath: "../assets/kidsImages/AlihanT.jpg",
+      imgPath: "./assets/kidsImages/AlihanT.jpg",
     },
     {    
-      imgPath: "../assets/kidsImages/Beria.jpg",
+      imgPath: "./assets/kidsImages/Beria.jpg",
     },
     {    
-      imgPath: "../assets/kidsImages/Berkan.jpg",
+      imgPath: "./assets/kidsImages/Berkan.jpg",
     },
     {    
-      imgPath: "../assets/kidsImages/Daniel3.jpg",
+      imgPath: "./assets/kidsImages/Daniel3.jpg",
     },
     {    
-      imgPath: "../assets/kidsImages/florian.jpg",
+      imgPath: "./assets/kidsImages/florian.jpg",
     },
     {    
-      imgPath: "../assets/kidsImages/Helena.jpg",
+      imgPath: "./assets/kidsImages/Helena.jpg",
     },
     {    
       imgPath: "../assets/kidsImages/jakob.jpg",
     },
     {    
-      imgPath: "../assets/kidsImages/Jan-Hendrik.jpg",
+      imgPath: "./assets/kidsImages/Jan-Hendrik.jpg",
     },
     {    
-      imgPath: "../assets/kidsImages/Jan2.jpg",
+      imgPath: "./assets/kidsImages/Jan2.jpg",
     },
     {    
-      imgPath: "../assets/kidsImages/Jonas_lebende wellen.jpg",
+      imgPath: "./assets/kidsImages/Jonas_lebende wellen.jpg",
     },
     {    
-      imgPath: "../assets/kidsImages/Laurenz.jpg",
+      imgPath: "./assets/kidsImages/Laurenz.jpg",
     },
     {    
-      imgPath: "../assets/kidsImages/Lea.jpg",
+      imgPath: "./assets/kidsImages/Lea.jpg",
     },
     {    
-      imgPath: "../assets/kidsImages/Lena.jpg",
-    },
-   
-    {    
-      imgPath: "../assets/kidsImages/Lena2.jpg",
+      imgPath: "./assets/kidsImages/Lena.jpg",
     },
    
     {    
-      imgPath: "../assets/kidsImages/LisaD.jpg",
+      imgPath: "./assets/kidsImages/Lena2.jpg",
     },
    
     {    
-      imgPath: "../assets/kidsImages/Luca.jpg",
+      imgPath: "./assets/kidsImages/LisaD.jpg",
     },
    
     {    
-      imgPath: "../assets/kidsImages/Malte.jpg",
+      imgPath: "./assets/kidsImages/Luca.jpg",
+    },
+   
+    {    
+      imgPath: "./assets/kidsImages/Malte.jpg",
     },
     {    
-      imgPath: "../assets/kidsImages/Max.jpg",
+      imgPath: "./assets/kidsImages/Max.jpg",
     },
     {    
-      imgPath: "../assets/kidsImages/Merve.jpg",
+      imgPath: "./assets/kidsImages/Merve.jpg",
     },
     {    
-      imgPath: "../assets/kidsImages/Michelle.jpg",
+      imgPath: "./assets/kidsImages/Michelle.jpg",
     },
     {    
-      imgPath: "../assets/kidsImages/Onur.jpg",
+      imgPath: "./assets/kidsImages/Onur.jpg",
     },
     {    
-      imgPath: "../assets/kidsImages/stella.jpg",
+      imgPath: "./assets/kidsImages/stella.jpg",
     },
     {    
-      imgPath: "../assets/kidsImages/Yusuf.jpg",
+      imgPath: "./assets/kidsImages/Yusuf.jpg",
     },
    
   ]
